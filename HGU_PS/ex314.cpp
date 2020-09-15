9f65146e66016c3461946670f72e2fa23353fd6c
heeseok <dldydldy@naver.com>
2020-09-15 14:09:53 +0900

    create ex314.cpp

---
+++ HGU_PS/ex314.cpp
@@ -0,0 +1,22 @@
+include <iostream>
+using namespace std;
+
+int fun(int n, int x) {
+  if(n%3>=0) {
+
+  } else if(n%4==0) {
+
+  } else {
+
+  }
+  return z
+}
+
+int main() {
+  int n, x, result;
+  scanf("%d", &n);
+  result = fun(n, x);
+
+  print("%d", result);
+  return 0;
+}
