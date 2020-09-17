71bfe553eebc38f80f472d484e48ae5fe01b926a
heeseok <dldydldy@naver.com>
2020-09-18 00:28:33 +0900

    solve P3

--- HGU_PS/P3.cpp
+++ HGU_PS/P3.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int n;
 
-struct Point {
-  int x;
-  int y;
+struct Carrier {
+  bool full;
+  int val;
 };
 
 void swap(int *a, int *b) {
@@ -45,28 +45,60 @@ void quick_sort_des(vector<int> &arr, int start, int end) {
 }
 
 int main() {
-  int m, i, j, temp, result, count, size;
+  int m, i, j, temp, result, size, over, under;
   vector<int> arr;
 
   scanf("%d", &m);
-  cout << "m : " << m << endl;
+  over = 0;
   while(cin >> temp) {
-  // while(scanf("%d", &temp) != cin.eof()) {
+    if(temp > m/2) {
+      over++;
+    }
     arr.push_back(temp);
   }
-  // size = i;
-  // cout << "size : " << size << endl;
   size = arr.size();
+  under = size - over;
   arr.resize(size);
-  for(i=0; i<size; i++) {
-    cout << arr[i];
-  }
-  cout << endl;
+
+  // for(i=0; i<size; i++) {
+  //   cout << arr[i] << " ";
+  // }
+  // cout << endl;
+
 
   quick_sort_des(arr, 0, size-1);
 
-  for(i=0; i<size; i++) {
-    cout << arr[i];
+  // for(i=0; i<size; i++) {
+  //   cout << arr[i] << " ";
+  // }
+  // cout << endl;
+  // cout << "1 : " << result << endl;
+  vector<bool> full(over);
+  for(i = over; i < size; i++) {
+    temp = arr[i];
+    for(j = 0; j < over; j++) {
+      if(temp + arr[j] <= m && full[j] == false) {
+        // cout << "temp : " << temp << endl;
+        under--;
+        full[j] = true;
+        break;
+      }
+    }
   }
-  cout << endl;
+  if(under%2 == 0) {
+    result = under/2;
+  } else {
+    result = under/2 + 1;
+  }
+  // cout << "2 : " << result << endl;
+  // cout << "over : " << over << endl;
+  // cout << "under : " << under << endl;
+  // cout << "under after : " << result << endl;
+  result += over;
+  // cout << "3 : " << result << endl;
+  cout << result;
+  // cout << result << endl;
+  // cout << endl;
+  // return 0;
+  exit(0);
 }
