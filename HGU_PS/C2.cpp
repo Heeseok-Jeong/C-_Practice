64d848b6bbbd7f56d61d9408098315bbe10debed
heeseok <dldydldy@naver.com>
2020-09-17 14:28:53 +0900

    solve C2 with DP

--- HGU_PS/C2.cpp
+++ HGU_PS/C2.cpp
@@ -1,163 +1,85 @@
 #include <iostream>
-#include <list>
 using namespace std;
 
 int n;
 
 struct Point {
-  int x;
-  int y;
+  int val;
+  bool visited;
+  int memo;
 };
 
-void swap(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+int min(int a, int b) {
+  if(a > b) return b;
+  else return a;
 }
 
-int max(int a, int b) {
-  if(a > b) return a;
-  else return b;
-}
-
-void quick_sort_des(int arr[250000], int start, int end, Point order[250000]) {
-  if(start >= end) {
-    return;
-  }
-  int pivot = start;
-  int up = start+1;
-  int down = end;
-  while(up <= down) {
-    while(up <= end && arr[up] >= arr[pivot]) {
-      up++;
-    }
-    while(down > start && arr[down] <= arr[pivot]) {
-      down--;
-    }
-    if(up > down) {
-      swap(arr[down], arr[pivot]);
-      swap(order[down], order[pivot]);
-    } else {
-      swap(arr[up], arr[down]);
-      swap(order[up], order[down]);
-    }
-  }
-    quick_sort_des(arr, start, down-1, order);
-    quick_sort_des(arr, down+1, end, order);
-}
-
-int compute_difficulty(int arr[][500], int start, int row, int col, int result) {
-  int val = arr[row][col];
+int compute_difficulty(Point arr[][500], int row, int col) {
   // cout << "row : " << row <<", col : " << col << endl;
-  if(val == -1) {
-    return result;
-  }
+  if(arr[row][col].visited) return arr[row][col].memo;
+  int val = arr[row][col].val;
+  int temp = val;
+  arr[row][col].visited = true;
 
   //상
-  if(row != 0 && arr[row-1][col] < val) {
-    result = max(result, compute_difficulty(arr, start, row-1, col, result));
+  if(row != 0 && arr[row-1][col].val < val) {
+    temp = min(temp, compute_difficulty(arr, row-1, col));
   }
   // 하
-  if(row != n-1 && arr[row+1][col] < val) {
-    result = max(result, compute_difficulty(arr, start, row+1, col, result));
+  if(row != n-1 && arr[row+1][col].val < val) {
+    temp = min(temp, compute_difficulty(arr, row+1, col));
   }
   // 좌
-  if(col != 0 && arr[row][col-1] < val) {
-    result = max(result, compute_difficulty(arr, start, row, col-1, result));
+  if(col != 0 && arr[row][col-1].val < val) {
+    temp = min(temp, compute_difficulty(arr, row, col-1));
   }
   // 우
-  if(col != n-1 && arr[row][col+1] < val) {
-    result = max(result, compute_difficulty(arr, start, row, col+1, result));
+  if(col != n-1 && arr[row][col+1].val < val) {
+    temp = min(temp, compute_difficulty(arr, row, col+1));
   }
-  // cout << result << endl;
-  arr[row][col] = -1;
-  return max(result, start-val);
+
+  arr[row][col].memo = temp;
+  return arr[row][col].memo;
 }
 
 int main() {
-  int i, j, result, row, col, val, gap, small, temp;
+  int i, j, temp, result = 0;
   scanf("%d", &n);
-  int arr[500][500], arr2[250000];
-  Point order[250000], p;
-  list<Point> stack;
+  Point arr[500][500];
 
   //입력
   for(i = 0; i < n; i++) {
     for(j = 0; j < n; j++) {
       scanf("%d", &temp);
-      arr[i][j] = temp;
-      arr2[n*i+j] = temp;
-      p.x = i;
-      p.y = j;
-      order[n*i+j] = p;
+      arr[i][j].val = temp;
     }
   }
 
-  // for(i=0;i<n*n;i++) {
-  //   cout << order[i].x << " " << order[i].y << ", ";
+  for(i = 0; i < n; i++) {
+    for(j = 0; j < n; j++) {
+      temp = arr[i][j].val - compute_difficulty(arr, i, j);
+      if(result < temp) {
+        result = temp;
+      }
+      // cout << "i : " << n*i+j << ", temp : " << temp << ", result : " << result << endl;
+    }
+  }
+
+  // for(i=0;i<n;i++) {
+  //   for(j=0;j<n;j++) {
+  //     cout << arr[i][j].val << " ";
+  //   }
+  // cout << endl;
   // }
   // cout << endl;
 
-  //값순서 내림차순 정렬 -> order[0]에는 값이 가장 큰 좌표 [x, y] 있음
-  quick_sort_des(arr2, 0, n*n-1, order);
-  // for(i=0;i<n*n;i++) {
-  //   scanf("%d %d", &p.x, &p.y);
-  //   order[i] = p;
-  // }
 
-
-  // for(i=0;i<n*n;i++) {
-  //   cout << order[i].x << " " << order[i].y << ", ";
-  // }
-  // cout << endl;
 // 4
 // 1 6 10 3
 // 5 5 6 1
 // 5 7 2 3
 // 11 5 7 4
-// 3 0
-// 0 2
-// 2 1
-// 3 2
-// 0 1
-// 1 2
-// 1 0
-// 1 1
-// 2 0
-// 3 1
-// 3 3
-// 0 3
-// 2 3
-// 2 2
-// 0 0
-// 1 3
 
-  //order 순서대로 돌기
-  small = arr[order[n*n-1].x][order[n*n-1].y];
-  for(i = 0; i < n*n; i++) {
-    // cout << "i : " << i << endl;
-    row = order[i].x;
-    col = order[i].y;
-    val = arr[row][col];
-    if(val == -1) {
-      continue;
-    }
-    gap = val - small;
-
-    //종료조건
-    if(result >= gap) {
-      // for(j=0;j<n;j++) {
-      //   for(int k=0;k<n;k++) {
-      //     cout << arr[j][k] << " ";
-      //   }
-      //   cout << endl;
-      // }
-      printf("%d\n", result);
-      return 0;
-    }
-
-    result = max(result, compute_difficulty(arr, val, row, col, result));
-  }
   printf("%d\n", result);
   return 0;
 }
