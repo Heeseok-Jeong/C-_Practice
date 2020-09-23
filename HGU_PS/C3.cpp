e9e0238bc9012fe19e62629cf618aa81f6c82394
heeseok <dldydldy@naver.com>
2020-09-23 20:02:54 +0900

    update C3.cpp

--- HGU_PS/C3.cpp
+++ HGU_PS/C3.cpp
@@ -11,11 +11,9 @@ void BFS(int start, int n, vector< vector<int> >& info, vector<int>& dis) {
   q.push(start);
   visited[start] = true;
 
-  // cout << start << " : ";
   while(!q.empty()) {
     int temp = q.front();
     q.pop();
-    // cout << temp << " ";
     for(it = info[temp].begin(); it != info[temp].end(); it++) {
       if(!visited[*it]) {
         q.push(*it);
@@ -24,7 +22,6 @@ void BFS(int start, int n, vector< vector<int> >& info, vector<int>& dis) {
       }
     }
   }
-  // cout << endl;
 }
 
 int main() {
@@ -32,7 +29,6 @@ int main() {
   vector< vector<int> > info;
   vector<int> A_dis, B_dis, N_dis;
 
-  //입력
   cin >> p >> q >> r >> n >> m;
   info.resize(n+1);
   A_dis.resize(n+1, 0);
@@ -44,13 +40,11 @@ int main() {
     info[b].push_back(a);
   }
 
-  //BFS
   BFS(1, n, info, A_dis);
   BFS(2, n, info, B_dis);
   BFS(n, n, info, N_dis);
 
 
-  // 최저 사용량 계산
   result = p*A_dis[1] + q*B_dis[1] + r*N_dis[1];
   for(i = 2; i < n+1; i++) {
     a = p*A_dis[i] + q*B_dis[i] + r*N_dis[i];
@@ -59,33 +53,4 @@ int main() {
     }
   }
   printf("%d\n", result);
-
-  // cout << "A_dis : ";
-  // for(vector<int>::iterator it = A_dis.begin(); it != A_dis.end(); it++) {
-  //   cout << *it << " ";
-  // }
-  // cout << endl;
-  //
-  // cout << "B_dis : ";
-  // for(vector<int>::iterator it = B_dis.begin(); it != B_dis.end(); it++) {
-  //   cout << *it << " ";
-  // }
-  // cout << endl;
-  //
-  // cout << "N_dis : ";
-  // for(vector<int>::iterator it = N_dis.begin(); it != N_dis.end(); it++) {
-  //   cout << *it << " ";
-  // }
-  // cout << endl;
-
-
-
-  // // 입력 체크
-  // for(i = 0; i < n+1; i++) {
-  //   cout << i << " : ";
-  //   for(vector<int>::iterator it = info[i].begin(); it != info[i].end(); it++) {
-  //     cout << *it << " ";
-  //   }
-  //   cout << endl;
-  // }
 }
