256dfb2b46d0a1db8213288ba22532ca1038f2aa
heeseok <dldydldy@naver.com>
2020-10-08 16:35:42 +0900

    solve C4

--- HGU_PS/C4.cpp
+++ HGU_PS/C4.cpp
@@ -1,100 +1,50 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <math.h>
 
 using namespace std;
 
 int main() {
-  int n, k, x, i, j, l, r, diff, min, temp = 0;
-  int l_diff, r_diff, l_target, r_target, count = 0;
-  vector<int> poles, selected;
-  vector<int>::iterator it, it2;
+  int n, k, x, i, left, right, predict, result = 0;
+  int latest_amp, count_amp, distance, smallest_distance = 0;
+  int poles[100000];
 
   scanf("%d %d", &n, &k);
   for(i = 0; i < n; i++) {
     scanf("%d", &x);
-    poles.push_back(x);
+    poles[i] = x;
   }
-  sort(poles.begin(), poles.end());
+  sort(poles, poles+n);
 
-  // cout << endl;
-  // for(it=poles.begin(); it != poles.end(); it++) {
-  //   cout << *it << endl;
-  // }
-  // cout << endl;
+  left = 1;
+  right = poles[n-1] - poles[0];
+  while(left <= right) {
+    predict = (left+right)/2;
+    latest_amp = 0;
+    count_amp = 1;
+    distance = 0;
+    smallest_distance = right;
 
-  // cout << "diff : " << diff << endl;
-  if(n == k) {
-    selected = poles;
-  }
-  else {
-    selected.push_back(poles[0]);
-    selected.push_back(poles[n-1]);
-    l = 0;
-    r = n-1;
-    count = k-2;
-    diff = (poles[r]-poles[l]) / (k-1);
-    // cout << "min : " << min << ", diff : " << diff << endl;
-    while(count > 0) {
-      // cout << "count : " << count << endl;
-      l_target = poles[l] + diff;
-      r_target = poles[r] - diff;
-      l_diff = diff;
-      r_diff = diff;
-      i = l;
-      i++;
-      temp = abs(l_target - poles[i]);
-      while(temp < l_diff) {
-        l_diff = temp;
-        i++;
-        temp = abs(l_target - poles[i]);
+    for(i = 1; i < n; i++) {
+      distance = poles[i] - poles[latest_amp];
+      if(predict <= distance) {
+        if(smallest_distance > distance) {
+          smallest_distance = distance;
+        }
+        latest_amp = i;
+        count_amp++;
       }
-      i--;
-
-      j = r;
-      j--;
-      temp = abs(r_target - poles[j]);
-      while(temp < r_diff) {
-        r_diff = temp;
-        j--;
-        temp = abs(r_target - poles[j]);
-      }
-      j++;
-
-      // cout << "l : " << l << ", i : " << i << ", l_diff : " << l_diff << endl;
-      // cout << "r : " << r << ", j : " << j <<", r_diff : " << r_diff << endl;
-      if(l_diff < r_diff) {
-        selected.push_back(poles[i]);
-        l = i;
-      }
-      else {
-        selected.push_back(poles[j]);
-        r = j;
-      }
-      // cout << min << endl;
-      count--;
     }
-    sort(selected.begin(), selected.end());
-  }
 
-
-  cout << endl << "selected" << endl;
-  for(it=selected.begin(); it != selected.end(); it++) {
-    cout << *it << endl;
-  }
-  cout << endl;
-
-  it = selected.begin();
-  it2 = it++;
-  min = poles[n-1]-poles[0];
-  for(; it != selected.end(); it++, it2++) {
-    if(min > *it-*it2) {
-      min = *it-*it2;
-      // cout << "min : " << min << endl;
+    if(count_amp >= k) {
+      if(result < smallest_distance) {
+        result = smallest_distance;
+      }
+      left = predict + 1;
+    }
+    else {
+      right = predict - 1;
     }
   }
-  // cout << endl;
-  printf("%d\n", min);
+  printf("%d\n", result);
   return 0;
 }
