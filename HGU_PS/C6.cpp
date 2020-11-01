a6b83fe4d9625b77be8a4452c6d0da12d20bf041
heeseok <dldydldy@naver.com>
2020-11-01 21:13:44 +0900

    save

--- HGU_PS/C6.cpp
+++ HGU_PS/C6.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
   int n  = 0, i = 0, j = 0, val = 0, asc_count = 0, des_count = 0, max_count = 0;
-  vector<int> cards, asc_list, asc_count_list, des_list, des_count_list;
+  vector<int> cards, asc_v, asc_count_v, des_v, des_count_v;
   scanf("%d", &n);
 
   for(i = 0; i < n; i++) {
@@ -16,55 +16,55 @@ int main() {
   for(i = 0; i < n; i++) {
     val = cards[i];
     if(asc_count == 0) {
-      asc_list.push_back(val);
+      asc_v.push_back(val);
       asc_count += 1;
     }
     else {
-      if(val > asc_list[asc_count-1]) {
-        asc_list.push_back(val);
+      if(val > asc_v[asc_count-1]) {
+        asc_v.push_back(val);
         asc_count += 1;
       }
       else {
         for(j = asc_count-2; j >= 0; j--) {
-          if(val > asc_list[j]) {
-            asc_list[j+1] = val;
+          if(val > asc_v[j]) {
+            asc_v[j+1] = val;
             break;
           }
         }
         if(j == -1)
-          asc_list[j+1] = val;
+          asc_v[j+1] = val;
       }
     }
-    asc_count_list.push_back(asc_count);
+    asc_count_v.push_back(asc_count);
 
     val = cards[n-1-i];
     if(des_count == 0) {
-      des_list.push_back(val);
+      des_v.push_back(val);
       des_count += 1;
     }
     else {
-      if(val > des_list[des_count-1]) {
-        des_list.push_back(val);
+      if(val > des_v[des_count-1]) {
+        des_v.push_back(val);
         des_count += 1;
       }
       else {
         for(j = des_count-2; j >= 0; j--) {
-          if(val > des_list[j]) {
-            des_list[j+1] = val;
+          if(val > des_v[j]) {
+            des_v[j+1] = val;
             break;
           }
         }
         if(j == -1)
-          des_list[j+1] = val;
+          des_v[j+1] = val;
       }
     }
 
-    des_count_list.push_back(des_count);
+    des_count_v.push_back(des_count);
   }
 
   for(i = 0; i < n; i++) {
-    if(max_count < asc_count_list[i] + des_count_list[n-1-i])
-      max_count = asc_count_list[i] + des_count_list[n-1-i]-1;
+    if(max_count < asc_count_v[i] + des_count_v[n-1-i])
+      max_count = asc_count_v[i] + des_count_v[n-1-i]-1;
   }
 
   printf("%d\n", max_count);
--- HGU_PS/P7.cpp
+++ HGU_PS/P7.cpp
@@ -4,18 +4,80 @@
 using namespace std;
 
 int main() {
-  vector<int> v1(6, 1);
-  vector<int> v2(7, 10);
+  int n, i, p, spent_time;
+  vector<int> p_v;
+  scanf("%d", &n);
+  for(i = 0; i < n; i++) {
+    scanf("%d", &p);
+    p_v.push_back(p);
+  }
 
-  for(auto item : v2)
-    cout << item << " ";
-  cout << endl;
+  int cnt_p = n;
+  while(true) {
+    vector<int> temp_p_v;
+    if(cnt_p == 1) {
+      printf("%d\n", spent_time);
+      break;
+    }
+    else if(cnt_p == 2) {
+      spent_time += p_v[0] + p_v[1];
+      temp_p_v.push_back(p_v[0] + p_v[1]);
+      cnt_p -= 1;
+    }
+    else if(cnt_p == 3) {
+      if(p_v[0] < p_v[2]) {
+        spent_time += p_v[0] + p_v[1];
+        temp_p_v.push_back(p_v[0] + p_v[1]);
+        temp_p_v.push_back(p_v[2]);
+      }
+      else {
+        spent_time += p_v[1] + p_v[2];
+        temp_p_v.push_back(p_v[0]);
+        temp_p_v.push_back(p_v[1] + p_v[2]);
+      }
+      cnt_p -= 1;
+    }
+    else {
+      int min_four_sum = p_v[0] + p_v[1] + p_v[2] + p_v[3];
+      int min_four_sum_i = 0;
+      for(i = 1; i < cnt_p-3; i++) {
+        if(min_four_sum > p_v[i] + p_v[i+1] + p_v[i+2] + p_v[i+3]) {
+            min_four_sum = p_v[i] + p_v[i+1] + p_v[i+2] + p_v[i+3];
+            min_four_sum_i = i;
+        }
+      }
+      vector<int> four_ele_sum_v;
+      four_ele_sum_v.push_back(2*(p_v[min_four_sum_i] + p_v[min_four_sum_i+1]) + p_v[min_four_sum_i+2]);
+      four_ele_sum_v.push_back(2*(p_v[min_four_sum_i+1] + p_v[min_four_sum_i+2]) + p_v[min_four_sum_i]);
+      four_ele_sum_v.push_back(2*(p_v[min_four_sum_i+1] + p_v[min_four_sum_i+2]) + p_v[min_four_sum_i+3]);
+      four_ele_sum_v.push_back(2*(p_v[min_four_sum_i+2] + p_v[min_four_sum_i+3]) + p_v[min_four_sum_i+1]);
+      four_ele_sum_v.push_back(p_v[min_four_sum_i] + p_v[min_four_sum_i+1] + p_v[min_four_sum_i+2] + p_v[min_four_sum_i+3]);
+      int min_sum = four_ele_sum_v[0];
+      for(i = 1; i < 5; i++) {
+        if(min_sum > four_ele_sum_v[i])
+          min_sum = four_ele_sum_v[i];
+      }
 
-  v2.assign(10, 5);
-
-  for(auto item : v2)
-    cout << item << " ";
-  cout << endl;
+      min_sum += p_v[min_four_sum_i] + p_v[min_four_sum_i+1] + p_v[min_four_sum_i+2] + p_v[min_four_sum_i+3];
+      spent_time += min_sum;
+      for(i = 0; i < min_four_sum_i; i++) {
+        temp_p_v.push_back(p_v[i]);
+      }
+      // temp_p_v.push_back(min_sum);
+      temp_p_v.push_back(p_v[min_four_sum_i] + p_v[min_four_sum_i+1] + p_v[min_four_sum_i+2] + p_v[min_four_sum_i+3]);
+      for(i = min_four_sum_i+4; i < cnt_p; i++) {
+        temp_p_v.push_back(p_v[i]);
+      }
+      cnt_p -= 3;
+      // cout << "min_f_s_i : " << min_four_sum_i << endl;
+      // cout << "cnt_p : " << cnt_p << endl;
+      // cout << "min_sum : " << min_sum << endl;
+    }
+    p_v = temp_p_v;
+    // for(i = 0; i < cnt_p; i++)
+    //   cout << p_v[i] << " ";
+    // cout << endl;
+  }
 
   return 0;
 }
