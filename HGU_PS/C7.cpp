c498f757ea02465c0a3bae656d3ea5361634f570
heeseok <dldydldy@naver.com>
2020-11-06 09:51:41 +0900

    C7 done

--- HGU_PS/C7.cpp
+++ HGU_PS/C7.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-vector<vector<int> > route_table;
+vector<int> route_list[10001];
 vector<int> parent_table, tree_height, cycle_elements_count_v;
 vector<bool> visited;
+bool is_odd, is_cycle;
 int n;
 
 int find_root(int x) {
@@ -39,25 +40,22 @@ bool check_cycle(int small, int large) {
     return true;
 }
 
-bool check_odd_cycle(int depth, int x, int target) {
+void check_odd_cycle(int depth, int x, int target) {
   if(x == target) {
+    is_cycle = true;
     if(depth%2 == 1)
-      return true;
-    else
-      return false;
+      is_odd = true;
+    return;
   }
 
-  for(int i = 1; i < n+1; i++) {
-    if(route_table[x][i] == 1 && !visited[i]) {
-      visited[i] = true;
-      if(check_odd_cycle(depth+1, i, target))
-        return true;
-      else
-        return false;
-      // visited[i] = false;
+  for(auto it = route_list[x].begin(); it != route_list[x].end(); it++) {
+    if(!visited[*it]) {
+      visited[*it] = true;
+      check_odd_cycle(depth+1, *it, target);
+      if(is_cycle)
+        return;
     }
   }
-  return false;
 }
 
 int main() {
@@ -65,57 +63,40 @@ int main() {
   scanf("%d", &n);
   scanf("%d", &m);
 
-  if(n == 1)
-    printf("0");
-  else if(n == 2) {
-    for(i = 0; i < m; i++) {
-      int small, large;
-      scanf("%d %d", &small, &large);
-      if(small > large) {
-        int temp = small;
-        small = large;
-        large = temp;
-      }
-      if(small == 1 && large == 2) {
-        printf("%d\n", i+1);
+  tree_height.assign(n+1, 1);
+  parent_table.resize(n+1);
+  for(i = 1; i < n+1; i++) {
+      parent_table[i] = i;
+  }
+  for(i = 0; i < n+1; i++) {
+
+  }
+
+  for(i = 0; i < m; i++) {
+    int small, large;
+    scanf("%d %d", &small, &large);
+    if(small > large) {
+      int temp = small;
+      small = large;
+      large = temp;
+    }
+
+    if(check_cycle(small, large)) {
+      is_odd = false;
+      is_cycle = false;
+      int depth = 1;
+      visited.assign(n+1, false);
+      visited[large] = true;
+      cycle_elements_count_v.clear();
+      check_odd_cycle(depth, large, small);
+      if(is_odd)
         break;
-      }
     }
+
+    route_list[small].push_back(large);
+    route_list[large].push_back(small);
   }
-  else {
-    tree_height.assign(n+1, 1);
-    parent_table.resize(n+1);
-    for(i =1; i < n+1; i++) {
-        parent_table[i] = i;
-    }
-    for(i = 0; i < n+1; i++) {
-      vector<int> route(n+1, 0);
-      route_table.push_back(route);
-    }
 
-    for(i = 0; i < m; i++) {
-      int small, large;
-      scanf("%d %d", &small, &large);
-      if(small > large) {
-        int temp = small;
-        small = large;
-        large = temp;
-      }
-
-      if(check_cycle(small, large)) {
-        int depth = 1;
-        visited.assign(n+1, false);
-        visited[large] = true;
-        cycle_elements_count_v.clear();
-        if(check_odd_cycle(depth, large, small))
-          break;
-      }
-
-      route_table[small][large] = 1;
-      route_table[large][small] = 1;
-    }
-
-    printf("%d\n", i+1);
-  }
+  printf("%d\n", i+1);
   return 0;
 }
