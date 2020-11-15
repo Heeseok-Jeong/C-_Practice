98dd7f32d75ca2eb6f0cc51551eeb09e563d5662
heeseok <dldydldy@naver.com>
2020-11-15 18:59:32 +0900

    Variable name changed in P9

--- HGU_PS/P9.cpp
+++ HGU_PS/P9.cpp
@@ -4,54 +4,54 @@
 using namespace std;
 
 int main() {
-  long long n, t, i, p, spd;
+  long long n, t, i, p, speed;
   scanf("%lld %lld", &n, &t);
   vector<pair<long long, long long> > people_info;
   for(i = 0; i < n; i++) {
-    scanf("%lld %lld", &p, &spd);
-    people_info.push_back(make_pair(p, spd));
+    scanf("%lld %lld", &p, &speed);
+    people_info.push_back(make_pair(p, speed));
   }
 
-  vector<long long> groups;
+  vector<long long> group_speed;
   for(i = 0; i < n; i++) {
-    groups.push_back(people_info[i].first + t*people_info[i].second);
+    group_speed.push_back(people_info[i].first + t*people_info[i].second);
   }
 
   bool is_changed = true;
   while(is_changed) {
-    long long groups_size = groups.size();
-    if(groups_size == 1)
+    long long group_speed_size = group_speed.size();
+    if(group_speed_size == 1)
       break;
 
     is_changed = false;
-    vector<long long> temp_group;
-    if(groups[0] >= groups[1]) {
-      temp_group.push_back(groups[1]);
+    vector<long long> temp_group_size;
+    if(group_speed[0] >= group_speed[1]) {
+      temp_group_size.push_back(group_speed[1]);
       is_changed = true;
     }
     else {
-      temp_group.push_back(groups[0]);
+      temp_group_size.push_back(group_speed[0]);
     }
 
-    for(i = 1; i < groups_size-1; i++) {
-      if(*temp_group.rbegin() != groups[i]) {
-        if(groups[i] >= groups[i+1]) {
-          temp_group.push_back(groups[i+1]);
+    for(i = 1; i < group_speed_size-1; i++) {
+      if(*temp_group_size.rbegin() != group_speed[i]) {
+        if(group_speed[i] >= group_speed[i+1]) {
+          temp_group_size.push_back(group_speed[i+1]);
           if(!is_changed)
             is_changed = true;
         }
         else {
-          temp_group.push_back(groups[i]);
+          temp_group_size.push_back(group_speed[i]);
         }
       }
     }
-    if(*temp_group.rbegin() < *groups.rbegin())
-      temp_group.push_back(*groups.rbegin());
+    if(*temp_group_size.rbegin() < *group_speed.rbegin())
+      temp_group_size.push_back(*group_speed.rbegin());
 
-    groups = temp_group;
+    group_speed = temp_group_size;
   }
 
-  printf("%lu\n", groups.size());
+  printf("%lu\n", group_speed.size());
 
   return 0;
 }
