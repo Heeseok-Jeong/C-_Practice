ab02bb604cf7082ec16fda03fb2b4806384f0ccf
heeseok <dldydldy@naver.com>
2020-11-18 12:36:11 +0900

    save C9

---
+++ HGU_PS/C9.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> root_table;
+
+int find(int x) {
+  if(x == root_table[x])
+    return x;
+  else
+    return root_table[x] = find(root_table[x]);
+}
+
+void merge(int target, int source) {
+  int root_t = find(target);
+  int root_s = find(source);
+  if(root_t > root_s) {
+    root_table[target] = root_s;
+  }
+  else if(root_t < root_s)
+    root_table[source] = root_t;
+}
+
+int main() {
+  int n, i;
+  scanf("%d", &n);
+  vector<pair<int, int> > p_v;
+  for(i = 0; i < n; i++) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    p_v.push_back(make_pair(x, y));
+  }
+
+  for(i = 0; i < n; i++) {
+    root_table.push_back(i);
+  }
+
+  for(i = 0; i < n; i++) {
+    for(int j = 0; j < i; j++) {
+      if((p_v[i].first > p_v[j].first && p_v[i].second < p_v[j].second) \
+        || (p_v[i].first < p_v[j].first && p_v[i].second > p_v[j].second))
+      {
+        cout << "i : " << i << ", j : " << j << endl;
+        merge(j, i);
+        for(int k = 0; k < n; k++)
+          cout << "k : " << k << ", table[k] : " << root_table[k] << endl;
+      }
+    }
+  }
+
+  set<int> disjoint_set;
+  for(i = 0; i < n; i++)
+    disjoint_set.insert(root_table[i]);
+
+  cout << disjoint_set.size() << endl;
+
+  return 0;
+}
