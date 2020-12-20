a2f66ebb6b725b742eca2bcc5ce8a5e8f00962cc
heeseok <dldydldy@naver.com>
2020-12-20 18:36:58 +0900

    change P11

--- HGU_PS/P11.cpp
+++ HGU_PS/P11.cpp
@@ -5,87 +5,71 @@
 
 using namespace std;
 
-vector<int> graph_arr[1000], memo[1000], visited, final_matching_words_index;
+vector<int> visited, final_matching_words_index;
+vector<string> words;
 bool exist_result = false;
 
-void browse_all_matching_words(int d, int n, int word_index, vector<int> matching_words_index) {
-  matching_words_index.push_back(word_index);
-  cout << "d = " << d << ", word_index = " << word_index << " || ";
-  for(auto x : matching_words_index) {
-    cout << x << ", ";
-  }
-  cout << endl;
-
+void browse(int x, int d, int n, vector<int> words_index) {
+  // cout << words[x] << endl;
+  // cout << "w_i : ";
+  // for(auto i : words_index) {
+  //   cout << words[i] << ", ";
+  // }
+  // cout << endl;
   if(d == n) {
-    if(exist_result) {
-      for(int i = 0; i < n; i ++) {
-        if(final_matching_words_index[i] < matching_words_index[i])
-          final_matching_words_index = matching_words_index;
-      }
-    }
-    else {
-      exist_result = true;
-      final_matching_words_index = matching_words_index;
-    }
-
+    exist_result = true;
+    final_matching_words_index = words_index;
+    // cout << "exit!" << endl;
     return;
   }
 
-  visited[word_index] = true;
-
   for(int i = 0; i < n; i++) {
-    if(i != word_index)
-      if(graph_arr[word_index][i] == 1 && !visited[i]) {
-        browse_all_matching_words(d+1, n, i, matching_words_index);
-        visited[i] = false;
-      }
+    if(i != x && words[x].back() == words[i].front() && !visited[i]) {
+      visited[i] = true;
+      words_index.push_back(i);
+      browse(i, d+1, n, words_index);
+      // cout << "out\n";
+      visited[i] = false;
+      words_index.pop_back();
+      if(exist_result)
+        return;
+    }
   }
 }
+
 
 int main() {
   int n, i, j;
   scanf("%d", &n);
   getchar();
-  vector<string> words;
+  
   for(i = 0; i < n; i++) {
     string str;
     getline(cin, str);
     words.push_back(str);
   }
   sort(words.begin(), words.end());
-  cout << endl;
-  for(i = 0; i < n; i ++)
-    cout << words[i] << ", ";
-  cout << endl;
+  // cout << endl;
+  // for(i = 0; i < n; i ++)
+  //   cout << words[i] << ", ";
+  // cout << endl;
 
-
+  
   for(i = 0; i < n; i++) {
-    graph_arr[i].assign(n, 0);
+    if(exist_result)
+      break;
+    visited.assign(n, false);
+    visited[i] = true;
+    vector<int> words_index;
+    words_index.push_back(i);
+    browse(i, 1, n, words_index);
   }
 
-  for(i = 0; i < n; i++) {
-    for(j = 0; j < n; j++) {
-      if(i != j)
-        if(words[i].back() == words[j].front())
-          graph_arr[i][j] = 1;
-    }
-  }
-
-  for(i = 0; i < n ; i++) {
-    for(j = 0; j < n; j++) {
-      cout << graph_arr[i][j] << ", ";
-    }
-    cout << endl;
-  }
-
-  visited.assign(n, 0);
-  vector<int> matching_words_index;
-  browse_all_matching_words(1, n, 0, matching_words_index);
+  // cout << "check" << endl;
 
   if(exist_result) {
-    for(i = 0; i < n; i++) {
-      cout << words[i] << endl;
-    }
+    for(auto i : final_matching_words_index)
+          cout << words[i] << endl;
   }
   else
     cout << 0 << endl;
